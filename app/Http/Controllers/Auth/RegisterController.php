bab914256e1e243f35a8fcbe7ea6458c92d32fdb
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-11-06 20:51:02 +0200

    Feat Add Middleware

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,6 +10,11 @@ use Illuminate\Validation\ValidationException;
 
 class RegisterController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['guest']);
+    }
+
     public function index()
     {
         return view('auth.register');
