fce83ea633664499458bbf8e44fc454eda14c438
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-11-06 10:53:02 +0200

    Feat Create User Login
    Make LoginController under auth dir
    index for login page
    store take user inputs for validation and redirecting depend on whether if auth user or not
    Create login form in login blade

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        //validate user info
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // log user in
+        if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
+            return back()->with('status', 'Invalid login details');
+        }
+
+        // after logged in direct to Dashboard
+        return redirect()->route('dashboard');
+    }
+}
