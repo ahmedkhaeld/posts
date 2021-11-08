27298df88990e9655a4c432248609d87e0caab0c
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-11-08 14:10:40 +0200

    Feat Add Pagination, Liked Unliked Posts
    Customize num of posts per page for possible fake data
    Make a Like&Unlike buttons
    Create likes table then migrate foreign keys
    create Like Model
    Create PostLikeController instantiate middleware for liked and liked cases, store the likes from users to a post, destroy likes
    User has many likes for posts but one per post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts=Post::get(); //collection
+        $posts=Post::Paginate(20); //collection
         return view('posts.index', [
             'posts'=>$posts
         ]);
